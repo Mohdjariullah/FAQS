0c6c1abc9d29aebe293c92edcd120d227c8c0dd4
Mohammed Jariullah Sami <112340610+Mohdjariullah@users.noreply.github.com>
2023-06-26 00:59:24 +0530

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,28 +1,28 @@
 const data = [{
   question: `How can I download the app?`,
-  answer: `To download the APK files, head over to the <a href="https://sangwan5688.github.io/download">download</a> section. There are download links present for every release. You can download the latest version for your device from there.`
+  answer: `To download the APK files, head over to the <a href="https://mohdjariullah.github.io/download">download</a> section. There are download links present for every release. You can download the latest version for your device from there.`
 }, {
   question: `What is the difference between the three links for Android and which one should I download?`,
-  answer: `Two architecture-specific (arm64-v8a and armeabi-v7a) and a Universal apk are provided with every release. Depending upon your device architecture (arm64 or armeabi) you can download the APK which supports your device. <a href="https://techbeyondboundaries.com/blogs/arm64_vs_armeabi/">Read our full guide</a>`
+  answer: `Two architecture-specific (arm64-v8a and armeabi-v7a) and a Universal apk are provided with every release. Depending upon your  device architecture (arm64 or armeabi) you can download the APK which supports your device. <a href="https://techbeyondboundaries.com/blogs/arm64_vs_armeabi/">Read our full guide</a>`
 }, {
   question: `How to install it on iOS`,
   answer: `You can use <a href="https://altstore.io/">altstore</a> or <a href="https://usescarlet.com/">scarlet</a> to sideload the app on iOS. Read our full guide on <a href="https://techbeyondboundaries.com/blogs/install_ipa_on_iphone/">how to install</a>.`
 }, {
-  question: `Is BlackHole on Play Store?`,
-  answer: `No, BlackHole isn't on PlayStore and is likely to remain so.`
+  question: `Is Cipher on Play Store?`,
+  answer: `No, Cipher isn't on PlayStore and is likely to remain so.`
 }, {
-  question: `Is BlackHole on F-Droid?`,
-  answer: `Yes, BlackHole is on F-Droid as well as IzzyDroid.`
+  question: `Is Cipher on F-Droid?`,
+  answer: `No, Cipher is not on F-Droid as well as IzzyDroid.`
 }, {
   question: `Why am I getting all Indian Songs in Search results, even after I changed the language setting to English.`,
-  answer: `First of all, Make sure you have selected the appropriate language in settings. Also, for some reason, The API which BlackHole uses shows Indian search results for every search which is made outside of India. To solve the issue you can either use a VPN with any Indian Server or use the youtube section of the app.`
+  answer: `First of all, Make sure you have selected the appropriate language in settings. Also, for some reason, The API which Cipher uses shows Indian search results for every search which is made outside of India. To solve the issue you can either use a VPN with any Indian Server or use the youtube section of the app.`
 }, {
   question: `While importing Playlists from Spotify I am getting all Indian Songs. Why so?`,
-  answer: `When you import any playlist from Spotify, BlackHole searches the songs of that playlist inside the app itself and then adds the top search result in the playlist. So, in short, the import depends upon search results. As described in the above issue, Make sure you have selected the appropriate language in settings. Also, if you're not from India, use a VPN with an Indian server.`
+  answer: `When you import any playlist from Spotify, Cipher searches the songs of that playlist inside the app itself and then adds the top search result in the playlist. So, in short, the import depends upon search results. As described in the above issue, Make sure you have selected the appropriate language in settings. Also, if you're not from India, use a VPN with an Indian server.`
 }, {
   question: `Can we download songs?`,
   answer: `Yup, you can even download songs.`
 }, {
   question: `What's the default encoding and song quality for downloading and streaming?`,
-  answer: `The songs are available in m4a format with 320 kbps max resolution in the main section, whereas, in weba format with 140 kbps (flexible) resolution in the Youtube section.`
+  answer: `The songs are available in m4a format with 320 kbps max resolution in the main section, whereas, in .weba format with 140 kbps (flexible) resolution in the Youtube section.`
 }]
